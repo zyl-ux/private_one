6228ffd6fe7d54520bfae2cc0ab404c912ea8efc
zyl-ux <57472404+zyl-ux@users.noreply.github.com>
2021-03-29 20:34:26 +0800

    Add files via upload

---
+++ JdbcUtils.java
@@ -0,0 +1,58 @@
+package com.ldq.utils;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.pool.DruidDataSourceFactory;
+
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class JdbcUtils {
+    private static DruidDataSource dataSource;
+
+    static {
+        Properties properties = new Properties();
+        try {
+            //读取jdbc.properties配置文件
+           InputStream inputStream = JdbcUtils.class.getClassLoader().getResourceAsStream("jdbc.properties");
+            properties.load(inputStream);
+            dataSource = (DruidDataSource) DruidDataSourceFactory.createDataSource(properties);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    /**
+     * 获取数据库连接池中的连接
+     * @return 如果返回null,说明获取连接失败<br/>有值就是获取连接成功
+     */
+    public static Connection getConnection(){
+
+        Connection conn = null;
+
+        try {
+            conn = dataSource.getConnection();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return conn;
+    }
+
+    /**
+     * 关闭连接，放回数据库连接池
+     * @param conn
+     */
+    public static void close(Connection conn){
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
